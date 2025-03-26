1852b966b3987855fd88d8ce73c6e31ca0f25749
Christopher Barnes <christopher@barnes.biz>
2025-03-26 12:33:56 +0100

    add provider with resource

---
+++ internal/provider/provider.go
@@ -0,0 +1,158 @@
+// Copyright (c) HashiCorp, Inc.
+
+package provider
+
+import (
+	"context"
+	"os"
+	roger "roger/internal/client"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+var (
+	_ provider.Provider = &rogerProvider{}
+)
+
+func New(version string) func() provider.Provider {
+	return func() provider.Provider {
+		return &rogerProvider{
+			version: version,
+		}
+	}
+}
+
+type rogerProviderModel struct {
+	Host types.String `tfsdk:"host"`
+	Port types.String `tfsdk:"Port"`
+}
+
+type rogerProvider struct {
+	version string
+}
+
+func (p *rogerProvider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "roger"
+	resp.Version = p.version
+}
+
+func (p *rogerProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Description: "Interact with roger.",
+		Attributes: map[string]schema.Attribute{
+			"host": schema.StringAttribute{
+				Description: "URI for roger API. May also be provided via ROGER_HOST environment variable.",
+				Optional:    true,
+			},
+			"port": schema.StringAttribute{
+				Description: "Port for roger API. May also be provided via ROGER_PORT environment variable.",
+				Optional:    true,
+			},
+		},
+	}
+}
+
+func (p *rogerProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	tflog.Info(ctx, "Configuring roger client")
+
+	var config rogerProviderModel
+	diags := req.Config.Get(ctx, &config)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	if config.Host.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("host"),
+			"Unknown roger API Host",
+			"The provider cannot create the roger API client as there is an unknown configuration value for the roger host. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the ROGER_HOST environment variable.",
+		)
+	}
+
+	if config.Port.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("port"),
+			"Unknown roger host Port",
+			"The provider cannot create the roger API client as there is an unknown configuration value for the roger port. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the ROGER_PORT environment variable.",
+		)
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	host := os.Getenv("ROGER_HOST")
+	port := os.Getenv("ROGER_PORT")
+
+	if !config.Host.IsNull() {
+		host = config.Host.ValueString()
+	}
+
+	if !config.Port.IsNull() {
+		port = config.Port.ValueString()
+	}
+
+	if host == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("host"),
+			"Missing roger API Host",
+			"The provider cannot create the roger API client as there is a missing or empty value for the roger host. "+
+				"Set the host value in the configuration or use the ROGER_HOST environment variable. "+
+				"If either is already set, ensure the value is not empty.",
+		)
+	}
+
+	if port == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("port"),
+			"Missing roger Port",
+			"The provider cannot create the roger API client as there is a missing or empty value for the roger port. "+
+				"Set the port value in the configuration or use the ROGER_PORT environment variable. "+
+				"If either is already set, ensure the value is not empty.",
+		)
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	ctx = tflog.SetField(ctx, "roger_host", host)
+	ctx = tflog.SetField(ctx, "roger_port", port)
+
+	tflog.Debug(ctx, "Creating roger client")
+
+	client, err := roger.NewClient(&host, &port)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to Create roger API Client",
+			"An unexpected error occurred when creating the roger API client. "+
+				"If the error is not clear, please contact the provider developers.\n\n"+
+				"roger Client Error: "+err.Error(),
+		)
+		return
+	}
+
+	resp.DataSourceData = client
+	resp.ResourceData = client
+
+	tflog.Info(ctx, "Configured roger client", map[string]any{"success": true})
+}
+
+func (p *rogerProvider) Resources(_ context.Context) []func() resource.Resource {
+	return []func() resource.Resource{
+		NewStateResource,
+	}
+}
+
+func (p *rogerProvider) DataSources(_ context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{}
+}
---
+++ internal/provider/state_resource.go
@@ -0,0 +1,208 @@
+// Copyright (c) HashiCorp, Inc.
+
+package provider
+
+import (
+	"context"
+	"fmt"
+	roger "roger/internal/client"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var (
+	_ resource.Resource                = &stateResource{}
+	_ resource.ResourceWithConfigure   = &stateResource{}
+	_ resource.ResourceWithImportState = &stateResource{}
+)
+
+func NewStateResource() resource.Resource {
+	return &stateResource{}
+}
+
+type stateResourceModel struct {
+	ID          types.String `tfsdk:"id"`
+	Hostname    types.String `tfsdk:"hostname"`
+	Message     types.String `tfsdk:"message"`
+	AppState    types.String `tfsdk:"appstate"`
+	LastUpdated types.String `tfsdk:"last_updated"`
+}
+
+type stateResource struct {
+	client *roger.Client
+}
+
+func (r *stateResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_roger"
+}
+
+func (r *stateResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Description: "Manages an roger state.",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Description: "Numeric identifier of the state.",
+				Computed:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"last_updated": schema.StringAttribute{
+				Description: "Timestamp of the last Terraform update of the state.",
+				Computed:    true,
+			},
+			"hostname": schema.StringAttribute{
+				Description: "Name of the hostname that belongs to the state.",
+				Required:    true,
+			},
+			"message": schema.StringAttribute{
+				Description: "Alert Message",
+				Optional:    true,
+			},
+			"appstate": schema.StringAttribute{
+				Description: "Set to 'production', 'draining' or 'quiesce' which are current valid states. Has no effect on alarm status, but may be used to set application state.",
+				Required:    true,
+			},
+		},
+	}
+}
+
+func (r *stateResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan stateResourceModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	state, err := r.client.CreateState(plan.Hostname.String(), plan.Message.String(), plan.AppState.String())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error creating state",
+			"Could not create state, unexpected error: "+err.Error(),
+		)
+		return
+	}
+
+	plan.Hostname = types.StringValue(state.Hostname)
+	plan.Message = types.StringValue(state.Message)
+	plan.AppState = types.StringValue(state.AppState)
+	plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+
+	diags = resp.State.Set(ctx, plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *stateResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var readState stateResourceModel
+	diags := req.State.Get(ctx, &readState)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	state, err := r.client.GetState(readState.ID.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error Reading roger state",
+			"Could not read roger state ID "+readState.ID.ValueString()+": "+err.Error(),
+		)
+		return
+	}
+
+	readState.Hostname = types.StringValue(state.Hostname)
+
+	diags = resp.State.Set(ctx, &readState)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *stateResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan stateResourceModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	_, err := r.client.UpdateState(plan.Hostname.String(), plan.Message.String(), plan.AppState.String())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error Updating roger state",
+			"Could not update state, unexpected error: "+err.Error(),
+		)
+		return
+	}
+
+	statePtr, err := r.client.GetState(plan.ID.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error Reading roger state",
+			"Could not read roger state ID "+plan.ID.ValueString()+": "+err.Error(),
+		)
+		return
+	}
+
+	plan.Hostname = types.StringValue(statePtr.Hostname)
+	plan.Message = types.StringValue(statePtr.Message)
+	plan.AppState = types.StringValue(statePtr.AppState)
+	plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+
+	diags = resp.State.Set(ctx, plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *stateResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var state stateResourceModel
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	err := r.client.DeleteState(state.Hostname.String())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error Deleting roger state",
+			"Could not delete state, unexpected error: "+err.Error(),
+		)
+		return
+	}
+}
+
+func (r *stateResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*roger.Client)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *roger.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.client = client
+}
+
+func (r *stateResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
