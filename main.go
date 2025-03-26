e7d1ddeae3b425a51b0d442632f587825a510b9c
Christopher Barnes <christopher@barnes.biz>
2025-03-26 12:34:09 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"roger/internal/provider"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+)
+
+var (
+	version string = "dev"
+
+	// goreleaser can pass other information to the main package, such as the specific commit
+	// https://goreleaser.com/cookbooks/using-main.version/
+)
+
+func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		Address: "gitlab.cern.ch/ai-config-team/roger",
+		Debug:   debug,
+	}
+
+	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
